103558dc0018bf499f04f9a4b516fa20fd0be5f7
Alex White <awhit012@gmail.com>
2014-05-08 16:22:24 -0700

    add my_solution.js to excercize 2

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,33 +1,68 @@
-// I paired [by myself, with:] on this challenge.
+
+// This is a Solo Challenge. 
+
+// There is a section below where you will write your code.
+// Do not alter this object here.
 
 
+var terah = {
+  name: "Terah",
+  age: 32,
+  height: 66,
+  weight: 130,
+  hairColor: "brown",
+  eyeColor: "brown"
+};
+/* Pseudocode Section - write pseudocode for each challenge below.
+1. Define a variable adam and use object literal notation to assign this variable 
+   the value of a JavaScript Object object with no properties.
 
+   var adam =  {}
 
-// Pseudocode
-// 
-// 
-// 
-// 
+2. Give adam a name property with the value "Adam".
 
+    adam [name] = adam
+3. Add a spouse property to terah and assign it the value of adam.
+   terah[spouse]= adam
+
+4. Change the value of the terah weight property to 125.
+   tarah [weight] = 125
+5. Remove the eyeColor property from terah.
+    delete terah[eyecolor]
+6. Add a spouse property to adam and assign it the value of terah.
+      adam [spouse] = terah
+7. Add a children property to terah and and use object literal notation to assign 
+   this variable the value of a JavaScript Object object with no properties
+    terah[children] = []
+8. Add a carson property to the value of the terah children property and assign it 
+  the value of an object with the property name with a value of "Carson".
+    terah [children] = carson
+9. Add a carter property to the value of the terah children property and assign it 
+   the value of an object with the property name with a value of "Carter".
+    treah children += carter
+10. Add a colton property to the value of the terah children property and assign it 
+    the value of an object with the property name with a value of "Colton".
+    terah children colton
+11. Add a children property to adam and assign it the value of terah children.
+     adam [children] =  terah[children]
+
+*/
 
 // __________________________________________
 // Write your code below.
 
+var adam = {
+  
+};
 
+adam.name = 'Adam';
 
-
+//terah.spouse = 'Adam';
 
 
 
 // __________________________________________
-// Refactored Code: Include a refactored version (or justification of your original code) here. 
-
-
-
-
-
-// __________________________________________
-// Reflection: Use the reflection guidelines to write a reflection here. 
+// Reflection: Use the reflection guidelines
 // 
 // 
 // 
@@ -38,7 +73,6 @@
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
-
 function assert(test, message, test_number) {
   if (!test) {
     console.log(test_number + "false");
@@ -49,56 +83,70 @@ function assert(test, message, test_number) {
 }
 
 assert(
-  (typeof secretNumber === 'number'),
-  "The value of secretNumber should be a number.",
+  (adam instanceof Object),
+  "The value of adam should be an Object.",
   "1. "
 )
 
 assert(
-  secretNumber === 7,
-  "The value of secretNumber should be 7.",
+  (adam.name === "Adam"),
+  "The value of the adam name property should be 'Adam'.",
   "2. "
 )
 
 assert(
-  typeof password === 'string',
-  "The value of password should be a string.",
+  terah.spouse === adam,
+  "terah should have a spouse property with the value of the variable adam.",
   "3. "
 )
 
 assert(
-  password === "just open the door",
-  "The value of password should be 'just open the door'.",
+  terah.weight === 125,
+  "The terah weight property should be 125.",
   "4. "
 )
 
 assert(
-  typeof allowedIn === 'boolean',
-  "The value of allowedIn should be a boolean.",
+  terah.eyeColor === undefined,
+  "The terah eyeColor property should be removed.",
   "5. "
 )
 
 assert(
-  allowedIn === false,
-  "The value of allowedIn should be false.",
+  terah.spouse.spouse === terah,
+  "The terah spouse property's value spouse property should be terah.",
   "6. "
 )
 
 assert(
-  members instanceof Array,
-  "The value of members should be an array",
+  (terah.children instanceof Object),
+  "The value of the terah children property should be an Object.",
   "7. "
 )
 
 assert(
-  members[0] === "John",
-  "The first element in the value of members should be 'John'.",
+  terah.children.carson.name === "Carson",
+  "The terah children property should have a carson property with its own property name with a value of 'Carson'.",
   "8. "
 )
 
 assert(
-  members[3] === "Mary",
-  "The fourth element in the value of members should be 'Mary'.",
+  terah.children.carter.name === "Carter",
+  "The terah children property should have a carter property with its own property name with a value of 'Carter'.",
   "9. "
 )
 
+assert(
+  terah.children.colton.name === "Colton",
+  "The terah children property should have a colton property with its own property name with a value of 'Colton'.",
+  "10. "
+)
+
+assert(
+  adam.children === terah.children,
+  "The value of the adam children property should be the value of the terah children property",
+  "11. "
+)
+
+console.log("\nHere is your final terah object:")
+console.log(terah)
