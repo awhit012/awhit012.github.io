fb55b7c7cace018ccc10c32f2ce27e03d1f0dc7e
Alex White <awhit012@gmail.com>
2014-05-09 18:21:12 -0700

    Update and rename my_solution.js to gc1_user_stories.js

---
+++ week_3/5_group_project/gc1_user_stories.js
@@ -0,0 +1,9 @@
+// I want to calculate the sum of a list of numbers.
+
+// I want to get the sum of a list of numbers. 
+
+// I want to get the average of a list of numbers. 
+
+// I want to get the median of a list of numbers.
+
+// I want to get the median of a list of numbers, if the list has an odd or even amount of numbers.  
