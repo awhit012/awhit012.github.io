5ae78466fbbffc88ed6c5487acccb6b0494e632e
Alex White <awhit012@gmail.com>
2014-05-09 17:54:24 -0700

    update my_solution for game

--- week_3/4_design_code_combat_mission/my_solution.js
+++ week_3/4_design_code_combat_mission/my_solution.js
@@ -1,37 +1,48 @@
-// I worked [by myself, with:] on this challenge
+//shapee_goes_coded//
+//objective: avoid baddies, get to portal2//
+var map {};
+//creates map//
+var portal1 {};
+//creates start portal//
+var portal2 {};
+//creates end portal//
+var shapee {};
+//creates shapee//
+var bads {};
+//creates bads//
 
-// Your mission description:
-// 
-// 
-// 
+//map attributes//
 
-// Pseudocode
-// 
-// 
-// 
-// 
-// 
+map.portal1 = portal1;
+//portal1 and portal2 are attributes of map//
 
-// Initial Code
+map.portal2 = portal2;
 
+//shappee is attribute of map
+map.shapee = shapee;
+
+//bads are attributes of map//
+map.bads = bads;
 
 
+//portal attributes//
+portal1.location = left;
+
+poral2.location = right;
+//left is location attrubute of portal1, right is location attribute of portal2//
+
+//shape attributes//
+shapee.location = poral1.location
+//portal1 is location attribute of shappee
+
+var moves {};
+//creates moves object?
+
+shapee.moves = up, down, right, left;
+//I dunno//
+
+shapee.moves.speed= 100px per turn
 
 
 
-// Refactored Code
 
-
-
-
-
-
-// Reflection
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
