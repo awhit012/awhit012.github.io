2b19e3c64b01e6c56b2aac34189d115c8d8512e2
Alex White <awhit012@gmail.com>
2014-10-12 21:05:31 -0700

    add string splitting and array parsing method

--- sudoku_solver/sudoku.js
+++ sudoku_solver/sudoku.js
@@ -1,8 +1,29 @@
 function sudokuSolver(){
+  that = this;
+
   this.solve = function(puzzle){
-  console.log(puzzle)
+    that.puzzle = puzzle;
+    that.splitStringIntoArray(puzzle)
+
+  }
+
+  this.splitStringIntoArray = function(){
+    that.arrayOfBoxValues = that.puzzle.split("");
+
+    that.parseArray(that.arrayOfBoxValues);
 
   };
+
+  this.parseArray = function(arrayOfBoxValues){
+    that.parsedArray = [];
+    length = arrayOfBoxValues.length
+
+    for(var i = 0; i < length; i++){
+      that.parsedArray.push( parseInt(arrayOfBoxValues[i]) )
+    };
+
+    console.log(that.parsedArray)
+  }
 
 };
 
