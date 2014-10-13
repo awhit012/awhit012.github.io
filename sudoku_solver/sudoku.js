ada92c959df688845ff24f470867a828fc17484f
Alex White <awhit012@gmail.com>
2014-10-12 21:09:27 -0700

    adds devide into rows method

--- sudoku_solver/sudoku.js
+++ sudoku_solver/sudoku.js
@@ -22,7 +22,17 @@ function sudokuSolver(){
       that.parsedArray.push( parseInt(arrayOfBoxValues[i]) )
     };
 
-    console.log(that.parsedArray)
+    that.devideIntoRows(that.parsedArray);
+  }
+
+  this.devideIntoRows = function(array){
+    this.row1 = [that.parsedArray[0], that.parsedArray[1], that.parsedArray[2]]
+    this.row2 = [that.parsedArray[3], that.parsedArray[4], that.parsedArray[5]]
+    this.row3 = [that.parsedArray[6], that.parsedArray[7], that.parsedArray[8]]
+    this.allRows = [this.row1, this.row2, this.row3]
+
+    console.log(that.allRows);
+
   }
 
 };
