609f5f3e34952af4ec95bc60a50f15814cd2d2ad
Alex White <awhit012@gmail.com>
2014-10-15 13:03:14 -0700

    update from dev repo, speed optimizations

--- colors.js
+++ colors.js
@@ -1,13 +1,1 @@
-$( document ).ready(function() {
-  setInterval(changeColors, 8000);
-});
-
-var colors = ['#2EB1FF', '#22FF8F', '#FF9C22', '#FF5722'];
-
-
-var changeColors = function(){
-  var randomIndex = Math.floor(Math.random()*colors.length)
-  $('body').css("background-color", colors[randomIndex] );
-  $('#table').css("box-shadow", colors[randomIndex] )
-}
-
+$(document).ready(function(){setInterval(changeColors,8e3)});var colors=["#2EB1FF","#22FF8F","#FF9C22","#FF5722"];var changeColors=function(){var e=Math.floor(Math.random()*colors.length);$("body").css("background-color",colors[e])}
