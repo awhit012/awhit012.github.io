fbf4c6dce6b97891e6d0a9e4c52e96b22641c0bc
Alex White <awhit012@gmail.com>
2014-11-14 21:24:30 -0800

    nicer typeface, subtracted unnecessary bells and whistles

--- colors.js
+++ colors.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){setInterval(changeColors,8e3)});var colors=["#2EB1FF","#22FF8F","#FF9C22","#FF5722"];var changeColors=function(){var e=Math.floor(Math.random()*colors.length);$("body").css("background-color",colors[e])}
+// $(document).ready(function(){setInterval(changeColors,8e3)});var colors=["#2EB1FF","#22FF8F","#FF9C22","#FF5722"];var changeColors=function(){var e=Math.floor(Math.random()*colors.length);$("body").css("background-color",colors[e])}
