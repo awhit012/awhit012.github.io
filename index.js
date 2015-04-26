05d36d36b7d5bdd9bcec8e7a91b0ce4f83a7e488
Alex White <awhit012@gmail.com>
2015-04-26 00:55:13 -0700

    uses better background

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = function(){
   var element = document.body;
 
   element.addEventListener("mousemove", function(z){
-    var x = ((z.screenX * -1 / 20) - 600), y = ((z.screenY * -1 / 20) + 0);
+    var x = ((z.screenX * -1 / 20) - 500), y = ((z.screenY * -1 / 20) + 150);
     element.style.backgroundPosition = x + 'px ' + y + 'px';
     // console.log(z.target)
   });
